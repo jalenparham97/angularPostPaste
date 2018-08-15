89b0c4087f06d1ee3929af3997d94587e821eeef
jalen <jalenparham97@gmail.com>
2018-08-14 20:07:02 -0400

    added upvote and downvote methods to post controller

--- app/components/post.js
+++ app/components/post.js
@@ -11,9 +11,26 @@ const post = {
   <section class="post__container">
     <h2 class="post__container--title">{{ $ctrl.post.title }}</h2>
     <p class="post__container--text">{{ $ctrl.post.text }}</p>
+    <i class="fas fa-thumbs-up like--btn" ng-click="$ctrl.likeButton()"> {{ $ctrl.likeCount }}</i>
+    <i class="fas fa-thumbs-down dislike--btn" ng-click="$ctrl.disLikeButton()"> {{ $ctrl.disLikeCount }}</i>
     <i class="fas fa-times delete--btn" ng-click="$ctrl.deletePost($index)"></i>
   </section>
-  ` 
+  `,
+  
+  controller: function() {
+    const vm = this;
+    vm.likeCount = null;
+    vm.disLikeCount = null;
+
+    vm.likeButton = function() {
+      console.log(vm.likeCount);
+      vm.likeCount += 1;
+    }
+    vm.disLikeButton = function() {
+      console.log(vm.likeCount);
+      vm.disLikeCount += 1;
+    }
+  }
 }
 
 
